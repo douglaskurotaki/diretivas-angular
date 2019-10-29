fa5b6bb34b958fddf8da85ca240a72c1ccc1de01
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2019-10-29 01:16:27 -0300

    directive with elementRef and Renderer

--- src/app/shared/fundo-amarelo.directive.ts
+++ src/app/shared/fundo-amarelo.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[fundoAmarelo]'
 })
 export class FundoAmareloDirective {
 
-  constructor() { }
+	constructor(private elementRef: ElementRef, 
+		private renderer: Renderer) { 
+		// console.log(this.elementRef);
+		// this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+		this.renderer.setElementStyle(this.elementRef.nativeElement,
+			'background-color', 
+			'yellow');
+	}
 
 }
